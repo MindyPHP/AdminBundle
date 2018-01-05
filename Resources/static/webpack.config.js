73767ba57d4118677c3ab3af162e05f1efd31d9e
Falaleev Maxim <max@studio107.ru>
2018-01-05 14:41:05 +0300

    done ui

--- Resources/static/webpack.config.js
+++ Resources/static/webpack.config.js
@@ -5,14 +5,15 @@ Encore
     .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
+    .enableSassLoader()
     .autoProvidejQuery()
     .addEntry('js/app', !Encore.isProduction() ? ['./hmr.js', './js/app.js'] : './js/app.js')
     .addStyleEntry('css/app', './css/app.scss')
     .enablePostCssLoader();
 
-// if (Encore.isProduction()) {
-//     Encore.setPublicPath('https://my-cool-app.com.global.prod.fastly.net');
-//     Encore.setManifestKeyPrefix('build/');
-// }
+if (Encore.isProduction()) {
+    Encore.setPublicPath('/bundles/admin/build');
+    Encore.setManifestKeyPrefix('bundles/admin/build/');
+}
 
 module.exports = Encore.getWebpackConfig();
